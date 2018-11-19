940b8089c394529039b9268f58f559edb7184d04
bill.mark <2286449127@qq.com>
2018-11-19 15:50:37 +0800

    up

---
+++ app/js/home/novice.vue
@@ -0,0 +1,26 @@
+<template>
+	<Panel title="先手特权" :class="$style.panel">
+		<section :class="$style.content">
+			<div class="item">
+				<h4>下载APP<span>送888元礼包</span></h4>
+				<p>新手专享</p>
+			</div>
+			<div class="item">
+				<ul>
+					<li>c</li>
+					<li>d</li>
+				</ul>
+			</div>
+		</section>
+	</Panel>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style lang="scss" module>
+	
+</style>
